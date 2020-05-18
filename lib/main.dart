0a0ad471069202dac9d4af7b66b89feb033513cc
CAROLINE DE FREITAS PAULA <caroline.paula@infinity.local>
2020-05-18 17:34:03 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import './questionario.dart';
+import './resultado.dart';
+
+main() => runApp(PerguntaApp());
+
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
+  var _pontuacaoTotal = 0;
+  final _perguntas = const [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': [
+        {'texto': 'Preto', 'pontuacao': 5},
+        {'texto': 'Azul', 'pontuacao': 10},
+        {'texto': 'Roxo', 'pontuacao': 5},
+        {'texto': 'Verde', 'pontuacao': 1},
+      ],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': [
+        {'texto': 'Gato', 'pontuacao': 10},
+        {'texto': 'Cachorro', 'pontuacao': 2},
+        {'texto': 'Coelho', 'pontuacao': 2},
+        {'texto': 'Aranha', 'pontuacao': 1},
+      ],
+    },
+    {
+      'texto': 'Qual é o seu gênero musical favorito?',
+      'respostas': [
+        {'texto': 'Funk', 'pontuacao': 1},
+        {'texto': 'Pop', 'pontuacao': 6},
+        {'texto': 'Rock Grunge', 'pontuacao': 10},
+        {'texto': 'Indie', 'pontuacao': 10},
+      ],
+    }
+  ];
+
+  void _responder(int pontuacao) {
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+        _pontuacaoTotal += pontuacao;
+      });
+    }
+  }
+
+  void _reiniciarQuestionario() {
+    setState(() {
+      _perguntaSelecionada = 0;
+      _pontuacaoTotal = 0;
+    });
+  }
+
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
+  }
+
+// Função build
+  @override
+  Widget build(BuildContext context) {
+    // for (var textoResp in respostas) {
+    //   widgets.add(Resposta(textoResp, _responder));
+    // }
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: temPerguntaSelecionada
+            ? Questionario(
+                perguntas: _perguntas,
+                perguntaSelecionada: _perguntaSelecionada,
+                responder: _responder,
+              )
+            : Resultado(_pontuacaoTotal, _reiniciarQuestionario),
+      ),
+    );
+  }
+}
+
+class PerguntaApp extends StatefulWidget {
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
+  }
+}
